6a427b62cb7bd81f3ca373d405dcefd5b3890676
Liza23 <lizadahiya@cse.iitb.ac.in>
2020-12-23 13:49:08 +0530

    base-code-a*-added

---
+++ A*_Search_Algorithm/a*_algorithm.py
@@ -0,0 +1,10 @@
+import utils
+
+# pair = (i, j) for storing coordinates of cell => or maybe lets define a struct?
+# pair_2 = (f,(i,j)) => maybe lets define struct? here f = g + h and (i,j) are the coordinates of cell
+pair = (1,2)
+pair_2 = (1,(1,2))
+print(pair, pair_2)
+
+def a_star_algorithm():
+    return True
