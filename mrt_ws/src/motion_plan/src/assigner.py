Minor Bug Fix Explicit conversion to float in tan angle calculation

mrt_ws/src/motion_plan/src/assigner.py
@@ -39,12 +39,12 @@ def main_node():
             else:
                 q=(0,0,np.sin(np.pi * orient/(2*180)), np.cos(np.pi * orient/(2*180)))
                 #TODO Add a check if arrow found is nearly in the direction of the previous arrow(or add a warning if it is)
+                posx, posy, q = my_client.bot_to_map(posx, posy, q)#map frame
                 if my_client.is_complete(posx,posy,q):
                     rospy.loginfo("Already visited recently found Goal: " + str([posx,posy]))
                 else:
                     i = 1
                     #rospy.loginfo("\n arrow found at (in map frame): \n" + str(my_client.bot_to_map(posx, posy, q)))
-                    posx, posy, q = my_client.bot_to_map(posx, posy, q)#map frame
                     my_client.add_arrow(posx, posy, q, color=(0,1,0))#Add Rviz arrow marker, map frame
                     success = my_client.move_to_off_goal(posx,posy, q = q, frame = "map", off_dist = 1)
                     if success == True:

mrt_ws/src/motion_plan/src/functions.py
@@ -206,9 +206,9 @@ class client():
                               arrow_tail = tuple(np.average([rect[0], rect[3]], axis = 0).astype(int))
                               if arrow_tail[0]-arrow_tip[0]==0:#escape possible runtime error
                                   continue
-                              print("tip-tail tan angle: ", abs((arrow_tail[1]-arrow_tip[1])/(arrow_tail[0]-arrow_tip[0])))
+                              print("tip-tail tan angle: ", abs(float(arrow_tail[1]-arrow_tip[1])/(arrow_tail[0]-arrow_tip[0])))
                               #Check that tan of angle of the arrow in the image from horizontal is less than 0.2(we are expecting nearly horizontal arrows)(atan(0.2) = 11.31)
-                              if abs((arrow_tail[1]-arrow_tip[1])/(arrow_tail[0]-arrow_tip[0])) > 0.2:
+                              if abs(float(arrow_tail[1]-arrow_tip[1])/(arrow_tail[0]-arrow_tip[0])) > 0.2:
                                   continue#Discard it, not a horizontal arrow
                               #cv2.circle(img, arrow_tail, 3, (0, 0, 255), cv2.FILLED)
                               #cv2.circle(img, tuple(np.average([arrow_tail, arrow_tip], axis=0).astype(int)), 3, (0, 0, 255), cv2.FILLED)#arrow centre